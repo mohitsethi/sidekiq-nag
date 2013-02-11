6df2a07cb511a2a249c357ff0ca13eb224fb418b
Gary Greyling <greyling.gary@gmail.com>
2013-02-11 12:58:10 +0200

    exit

---
+++ lib/sidekiq-nag.rb
@@ -0,0 +1,7 @@
+require "sidekiq-nag/version"
+
+module Sidekiq
+  module Nag
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq-nag/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Nag
+    VERSION = "0.0.1"
+  end
+end
